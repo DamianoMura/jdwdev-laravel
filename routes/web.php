43f352e261c6cc9bda2da1ebda7d611865dd30a6
DamianoMura <mura.damiano14@gmail.com>
2025-12-08 14:13:03 +0100

    [sync]

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MoviesController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
 });
